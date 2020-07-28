2fc3edf76927d15513cf14e9eca66f7f9b1fe7b9
Arthur Siqueira <arthur.siq5@gmail.com>
2020-07-28 02:57:38 -0300

    criado componente React usando styled components

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ButtonLink from '../components/ButtonLink';
 import Logo from '../../assets/img/logo.png';
+import Button from '../Button';
 
 import './Menu.css';
 
@@ -11,9 +12,9 @@ export default function Menu(){
                 <img className="Logo" src={Logo} alt="TuiFlix logo" />
             </a>
 
-            <ButtonLink className="ButtonLink" url="/">
+            <Button as="a" className="ButtonLink" url="/">
                 Novo vídeo
-            </ButtonLink>
+            </Button>
         </nav>
     );
 }
