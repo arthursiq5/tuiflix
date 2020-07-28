4901c233b40809796f7da7695c3885d0259b8aa3
Arthur Siqueira <arthur.siq5@gmail.com>
2020-07-27 23:50:28 -0300

    inserido ícone do site no menu

--- src/Menu/index.jsx
+++ src/Menu/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from '../assets/img/logo.png'
 
 export default function Menu(){
     return (
         <nav>
             Tuiflix
+            <img src={Logo} alt="TuiFlix logo" />
         </nav>
     );
 }
