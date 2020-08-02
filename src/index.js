1c1de0eaffe4bc51b4ac3935ec971ec51085dfe1
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-02 13:07:24 -0300

    transferida página Main para pasta correta da estrutura

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from './pages/Main';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video';
 
 import './index.css';
 
-
-
-
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={App} />
+      <Route exact path="/" component={Main} />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route component={() => <div>Not found</div>} />
     </Switch>
