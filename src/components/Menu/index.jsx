a87bcc74198a77f460aba6d9c52b710d7d8af92b
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-02 22:37:35 -0300

    Utilizado Link para navegação entre rotas

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import Button from '../Button';
 
@@ -7,11 +8,11 @@ import './Menu.css';
 export default function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="TuiFlix logo" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" url="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
