afbadb817072f1107713aedf8f6954801eb8bd08
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-04 00:58:33 -0300

    Criado componente e rota para cadastro de categoria

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import Main from './pages/Main';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 
 ReactDOM.render(
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Switch>
       <Route exact path="/" component={Main} />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={() => <div>Not found</div>} />
     </Switch>
   </BrowserRouter>,
