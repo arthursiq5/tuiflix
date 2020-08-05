ebcee990235bb3eeaa9dded22e8e7b51ccf5c09d
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-05 14:45:58 -0300

    Criado formulário simples

--- src/pages/cadastro/Categoria/index.jsx
+++ src/pages/cadastro/Categoria/index.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import PageDefault from '../../../components/PageDefault'
 import { Link } from 'react-router-dom';
 
-export default function CadastroCategoria(){
-    return <PageDefault>
-        <h1>Cadastro de categoria</h1>
-        <Link to="/cadastro/video">
-            Cadastrar video
+export default function CadastroCategoria() {
+    return (
+        <PageDefault>
+            <h1>Cadastro de Categoria</h1>
+
+            <form>
+
+            <label>
+                Nome da Categoria:
+                <input
+                type="text"
+                />
+            </label>
+
+            <button>
+                Cadastrar
+            </button>
+            </form>
+
+
+            <Link to="/">
+            Ir para home
         </Link>
     </PageDefault>
+    );
 }
