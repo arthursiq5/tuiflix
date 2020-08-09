acec746c7e666ee9c3ae15ee2f47b6c743781f38
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-09 01:41:02 -0300

    utilizado state do componente para armazenamento de dados mostrado exemplo de uso do state

--- src/pages/cadastro/Categoria/index.jsx
+++ src/pages/cadastro/Categoria/index.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault'
 import { Link } from 'react-router-dom';
 
 export default function CadastroCategoria() {
+
+
+    const [categorias, setCategorias] = useState(['teste', 'exemplo']);
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('');
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-            <form>
+            <form onSubmit={function handleSubmit(event){
+                event.preventDefault();
+                console.log('Tentativa de envio do formulário');
+                setCategorias([
+                    ...categorias,
+                    nomeDaCategoria
+                ])
+            }}>
 
             <label>
                 Nome da Categoria:
                 <input
                 type="text"
+                value={nomeDaCategoria}
+                onChange={((dadosDoEvento) => {
+                        console.log("executou");
+                        console.log("[dados.target]", dadosDoEvento.target.value);
+                        setNomeDaCategoria(dadosDoEvento.target.value);
+                    }
+                )}
                 />
             </label>
 
@@ -21,10 +40,17 @@ export default function CadastroCategoria() {
             </button>
             </form>
 
+            <ul>
+                {categorias.map((categoria) => {
+                    return <li key={categoria}>
+                        {categoria}
+                    </li>
+                })}
+            </ul>
 
             <Link to="/">
             Ir para home
-        </Link>
-    </PageDefault>
+            </Link>
+        </PageDefault>
     );
 }
