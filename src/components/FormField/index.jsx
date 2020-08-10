b293e4308129e5ab8c3e8e075d32fa7fc16c29bf
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-09 21:40:36 -0300

    personalizado arquivo lint para atender às necessidades do projeto

--- src/components/FormField/index.jsx
+++ src/components/FormField/index.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react';
 
+// eslint-disable-next-line react/prop-types
 export default function FormField({ label, type, name, value, onChange }) {
-  return (
-    <div>
-      <label>
-        {label}:
-        <input
-          type={type}
-          value={value}
-          name={name}
-          onChange={onChange}
-        />
-      </label>
-    </div>
-  );
+    return (
+        <div>
+            <label>
+                {label}
+                :
+                <input
+                    type={type}
+                    value={value}
+                    name={name}
+                    onChange={onChange}
+                />
+            </label>
+        </div>
+    );
 }
